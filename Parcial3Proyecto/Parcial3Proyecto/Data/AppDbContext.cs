5e8680f12f39215fae23c668784ee814615af9b5
jgalvissaavedra <jgalvissaavedra@gmail.com>
2021-11-11 12:58:34 -0400

    incluido controklador

---
+++ Parcial3Proyecto/Parcial3Proyecto/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial3Proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial3Proyecto.Data
+{
+    public class AppDbContext : DbContext
+
+    {
+
+
+        public DbSet<Usuario> Usuarios { get; set; } //Llama al modelo (Person) 
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+
+
+
+        }
+
+    }
+}
